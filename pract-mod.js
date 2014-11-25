c58cae898f3a62fbf598e512377905fe42ccca78
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-11-24 23:25:10 -0800

    Some changes, solution additions. I am still working on 6. The callbacks in JS are tripping me out a little

---
+++ pract-mod.js
@@ -0,0 +1,4 @@
+module.exports = function (directory, extension, callback) {
+  // console.log();
+
+};
--- program5-13.js
+++ program5-13.js
@@ -18,3 +18,17 @@ fs.readdir(filePath, function(err, files) {
     }
   }
 });
+
+
+/*
+Official Solution.
+var fs = require('fs')
+    var path = require('path')
+
+    fs.readdir(process.argv[2], function (err, list) {
+      list.forEach(function (file) {
+        if (path.extname(file) === '.' + process.argv[3])
+          console.log(file)
+      })
+    })
+*/
